92f85213099e85fc901f054e1c0c97d34fdc9afe
Mikiko Bazeley <please try social media>
2024-06-24 21:53:07 -0700

    Fixed positiona; var

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ hf_embeddings = HuggingFaceEndpointEmbeddings(
 
 vectorstore = Qdrant.from_documents(
     documents=split_documents,
-    embedding_model=hf_embeddings,
+    embeddings=hf_embeddings,
     location=":memory:",
     collection_name="AirBnB_10K",
 )
